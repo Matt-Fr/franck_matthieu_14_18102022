6bd8ed82818c9e92fd9487a092b1d9c67c5f04cb
Matt-Fr <matthieu_franck@orange.fr>
2022-10-18 22:28:52 +0200

    added different pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Employees from "./pages/Employees";
--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Employees = () => {
-  return <div>Employees</div>;
+  return (
+    <main>
+      <div id="employee-div" class="container">
+        <h1>Current Employees</h1>
+        <table id="employee-table" class="display"></table>
+        <a href="index.html">Home</a>
+      </div>
+    </main>
+  );
 };
 
 export default Employees;
