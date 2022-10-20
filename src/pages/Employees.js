c18ba27e5ba782281d957217bf418c620017a00e
Matt-Fr <matthieu_franck@orange.fr>
2022-10-20 10:36:01 +0200

    added table

--- src/pages/Employees.js
+++ src/pages/Employees.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import EnhancedTableHead from "../components.js/EmployeesTable";
 
 const Employees = () => {
   return (
     <main>
       <div id="employee-div" className="container">
         <h1>Current Employees</h1>
-
+        <EnhancedTableHead></EnhancedTableHead>
         <Link to="/">Home</Link>
       </div>
     </main>
