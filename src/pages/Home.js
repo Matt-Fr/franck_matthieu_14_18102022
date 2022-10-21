bb715ccc7bc35510ca0538a532010bf958245d84
Matt-Fr <matthieu_franck@orange.fr>
2022-10-21 17:09:23 +0200

    added a few inputs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,29 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import MaterialUIPickers from "../components.js/DatePicker";
 import SelectSmall from "../components.js/Select";
 import { addEmployeeToList } from "../features/employeeSlice";
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { makeStyles } from "@material-ui/styles";
 
 const Home = () => {
+  const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
+
   const dispatch = useDispatch();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [startDate, setStartDate] = useState(null);
 
   const newEmployee = {
     firstName,
@@ -29,25 +44,51 @@ const Home = () => {
         <Link to="/employees">View Current Employees</Link>
         <h2>Create Employee</h2>
         <form action="#" id="create-employee" onSubmit={handleSubmit}>
-          <label htmlFor="first-name">First Name</label>
-          <input
+          {/* <label htmlFor="first-name">First Name</label> */}
+          {/* <input
             type="text"
             id="first-name"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
             required="required"
+          /> */}
+
+          <TextField
+            required="required"
+            value={firstName}
+            id="outlined-required"
+            label="First Name"
+            variant="outlined"
+            onChange={(e) => setFirstName(e.target.value)}
           />
 
-          <label htmlFor="last-name">Last Name</label>
-          <input
+          {/* <label htmlFor="last-name">Last Name</label> */}
+          {/* <input
             type="text"
             id="last-name"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
+          /> */}
+
+          <TextField
+            required="required"
+            value={lastName}
+            id="outlined-required"
+            label="First Name"
+            variant="outlined"
+            onChange={(e) => setLastName(e.target.value)}
           />
 
-          <label htmlFor="date-of-birth">Date of Birth</label>
-          {/* <MaterialUIPickers></MaterialUIPickers> */}
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Basic example"
+              value={startDate}
+              onChange={(newValue) => {
+                setStartDate(newValue);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
 
           <label htmlFor="start-date">Start Date</label>
           <input id="start-date" type="text" />
