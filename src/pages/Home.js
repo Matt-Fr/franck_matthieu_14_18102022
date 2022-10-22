3412a8d126216366469537a0e87e1c78e7b2ddde
Matt-Fr <matthieu_franck@orange.fr>
2022-10-22 15:22:56 +0200

    added all inputs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,6 @@ import TextField from "@mui/material/TextField";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { makeStyles } from "@material-ui/styles";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
@@ -17,15 +16,17 @@ const Home = () => {
   const dispatch = useDispatch();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [birthDate, setBirthDate] = useState(null);
   const [startDate, setStartDate] = useState(null);
   const [stateInput, setStateInput] = useState("");
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [zipCode, setzipCode] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(addEmployeeToList(newEmployee));
   };
-
-  console.log(stateInput);
 
   const newEmployee = {
     firstName,
@@ -44,15 +45,6 @@ const Home = () => {
         <Link to="/employees">View Current Employees</Link>
         <h2>Create Employee</h2>
         <form action="#" id="create-employee" onSubmit={handleSubmit}>
-          {/* <label htmlFor="first-name">First Name</label> */}
-          {/* <input
-            type="text"
-            id="first-name"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required="required"
-          /> */}
-
           <TextField
             required="required"
             value={firstName}
@@ -61,14 +53,6 @@ const Home = () => {
             variant="outlined"
             onChange={(e) => setFirstName(e.target.value)}
           />
-
-          {/* <label htmlFor="last-name">Last Name</label> */}
-          {/* <input
-            type="text"
-            id="last-name"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          /> */}
 
           <TextField
             required="required"
@@ -81,7 +65,18 @@ const Home = () => {
 
           <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DatePicker
-              label="Basic example"
+              label="Date of Birth"
+              value={birthDate}
+              onChange={(newValue) => {
+                setBirthDate(newValue);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
+
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label="Start Date"
               value={startDate}
               onChange={(newValue) => {
                 setStartDate(newValue);
@@ -90,20 +85,29 @@ const Home = () => {
             />
           </LocalizationProvider>
 
-          <label htmlFor="start-date">Start Date</label>
-          <input id="start-date" type="text" />
-
           <fieldset className="address">
             <legend>Address</legend>
-
-            <label htmlFor="street">Street</label>
-            <input id="street" type="text" />
-
-            <label htmlFor="city">City</label>
-            <input id="city" type="text" />
-
-            <label htmlFor="state">State</label>
-
+            <TextField
+              value={street}
+              id="outlined-required"
+              label="Street"
+              variant="outlined"
+              onChange={(e) => setStreet(e.target.value)}
+            />
+            <TextField
+              value={city}
+              id="outlined-required"
+              label="City"
+              variant="outlined"
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <TextField
+              value={zipCode}
+              id="outlined-required"
+              label="Zip Code"
+              variant="outlined"
+              onChange={(e) => setzipCode(e.target.value)}
+            />
             <FormControl sx={{ m: 1, minWidth: 80 }}>
               <InputLabel id="demo-simple-select-autowidth-label">
                 State
@@ -124,9 +128,6 @@ const Home = () => {
                 })}
               </Select>
             </FormControl>
-
-            <label htmlFor="zip-code">Zip Code</label>
-            <input id="zip-code" type="number" />
           </fieldset>
 
           <label htmlFor="department">Department</label>
